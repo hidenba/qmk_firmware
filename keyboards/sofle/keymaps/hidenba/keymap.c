f4441da1fca37352cf3ae179b8d6de6498aee7b3
HIDEKUNI Kajita <hide.nba@gmail.com>
2021-02-02 11:27:15 +0900

    add screenshot

--- keyboards/sofle/keymaps/hidenba/keymap.c
+++ keyboards/sofle/keymaps/hidenba/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSLS, \
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC, \
   KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    I3_LOCK,XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_MINS, \
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    I3_LOCK,I3_SP,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_MINS, \
                      KC_LCTRL,KC_LGUI, KC_LALT, KC_LOWER,KC_SPC, KC_ENT,  KC_RAISE,  KC_RALT, KC_RGUI, KC_RCTRL \
 ),
 [_LOWER] = LAYOUT( \
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_RAISE] = LAYOUT( \
   KC_ESC,  I3_S1,   I3_S2,   I3_S3,   I3_S4,   I3_S5,                     I3_S6,   I3_S7,   I3_S8,   I3_S9,   I3_S0,   XXXXXXX, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, I3_SP,   XXXXXXX, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, I3_SJ,   I3_SK,   I3_SL,   I3_SSCLN,XXXXXXX, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
                     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
